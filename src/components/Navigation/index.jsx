c5f9a30ad79f755a29e62127f10b8bf7e3159648
Pranshu Teotia <pranshu28@gmail.com>
2023-01-23 21:37:13 -0800

    Added some colors and styles

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+const Navigation = () => {
+  return(
+  <nav className="nav">
+    <p>Anurag Daimary</p>
+    <ul>
+      <li>About</li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Navigation;
