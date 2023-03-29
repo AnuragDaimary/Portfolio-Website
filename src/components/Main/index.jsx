1775309b9d8fd9e5fe4a5381a0971a813fb68b19
Pranshu Teotia <pranshu28@gmail.com>
2023-03-28 21:01:42 -0700

    Add animations to nav

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -16,6 +16,12 @@ const Main = () => {
     animate: "visible",
   }
 
+  const transitionProps = {
+    ease: [0.48,0.03,0.46,1.17],
+    delay: 0.5,
+    duration: 0.5,
+  }
+
   const greeting = ["Hi, ", "I am ", "Anurag"];
   const sentence1 = ["User ", "Experience ", "Designer"];
   const sentence2 = ["and ", "UX ",  "Researcher"];
@@ -32,19 +38,19 @@ const Main = () => {
               } else {
                 displayChunk = chunk;
               }
-              return <motion.span {...animateProps} key={`${chunk}-${idx}`} transition={{ease: [0.48,0.03,0.46,1.17], delay: 1 + (0.1 *idx), duration: 0.5}}>{displayChunk}</motion.span>
+              return <motion.span {...animateProps} key={`${chunk}-${idx}`} transition={{...transitionProps, delay: 0.5 + (0.1 * idx)}}>{displayChunk}</motion.span>
             })}
           </span>
           </h1>
         <h2>
           <span className="block clippy">
             {sentence1.map((chunk, idx) => {
-              return <motion.span {...animateProps} key={`${chunk}-${idx}`} transition={{ease: [0.48,0.03,0.46,1.17], delay: 0.8 + (0.1 *idx), duration: 1}}>{chunk}</motion.span>
+              return <motion.span {...animateProps} key={`${chunk}-${idx}`} transition={{...transitionProps, delay: 0.8 + (0.1 * idx)}}>{chunk}</motion.span>
             })}
           </span>
           <span className="block clippy">
             {sentence2.map((chunk, idx) => {
-              return <motion.span {...animateProps} key={`${chunk}-${idx}`} transition={{ease: [0.48,0.03,0.46,1.17], delay: 1.1 + (0.1 *idx), duration: 1}}>{chunk}</motion.span>
+              return <motion.span {...animateProps} key={`${chunk}-${idx}`} transition={{...transitionProps, delay: 1.1 + (0.1 * idx)}}>{chunk}</motion.span>
             })}
           </span>
         </h2>
