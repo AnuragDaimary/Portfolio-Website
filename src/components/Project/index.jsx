c4ea28c8c622515cdab6505f6cdeaac27055d5e0
Pranshu Teotia <pranshu28@gmail.com>
2023-03-31 20:45:58 -0700

    Add animations to work section

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,18 +1,56 @@
 import Card from "../Card"
 import Pill from "../Pill";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 
 const Project = ({ projectName, projectDesc, skills, inverted }) => {
+  const [ref, inView] = useInView({ amount: 1 });
+  const controls = useAnimation();
+
+  const sentence = {
+    hidden: { opacity: 0, y: 75},
+    visible: {
+      opacity: 1,
+      y: 0,
+    }
+  }
+
+  const animateProps = {
+    variants: sentence,
+    initial: "hidden",
+    animate: controls,
+    className: "block",
+  }
+
+  const transitionProps = {
+    ease: [0.25, 0.13, 0.39, 1],
+    delay: 1,
+    duration: 0.75,
+  }
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   const contentClassName = inverted ? 'project-content-inverted' : 'project-content';
   return (
     <div className={`project ${inverted ? 'flex-row-reverse justify-end': ''}`}>
-      <Card />
+      <motion.span {...animateProps} transition={transitionProps}>
+        <Card />
+      </motion.span>
       <div className={contentClassName}>
-        <h4>{projectName}</h4>
-        <p className="project-desc">{projectDesc.map(chunk => <p>{chunk}</p>)}</p>
+        <motion.h4 {...animateProps} transition={transitionProps}>{projectName}</motion.h4>
+        <div className="project-desc">{projectDesc.map((chunk, i) => <motion.p {...animateProps} transition={{...transitionProps, delay: 1.2 + (i * 0.1)}} key={`${chunk}-${i}`}>{chunk}</motion.p>)}</div>
         <div className="skill-container">
-          {skills.map(skill => (
-            <Pill skillName={skill} />
+          {skills.map((skill, i) => (
+            <motion.span {...animateProps} transition={{...transitionProps, delay: 1.4 + (i*0.1)}}>
+              <Pill key={`${skill}-${i}`} skillName={skill} />
+            </motion.span>
           ))}
+          <span ref={ref}></span>
         </div>
       </div>
     </div>
--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,9 +1,46 @@
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+
 import Project from "../Project";
+import { useEffect } from "react";
 
 const Work = () => {
+  const [ref, inView] = useInView({ amount: 1 });
+  const controls = useAnimation();
+
+  const sentence = {
+    hidden: { opacity: 1, y: 75},
+    visible: {
+      opacity: 1,
+      y: 0,
+    }
+  }
+
+  const animateProps = {
+    variants: sentence,
+    initial: "hidden",
+    animate: controls,
+    className: "block",
+  }
+
+  const transitionProps = {
+    ease: [0.25, 0.13, 0.39,1],
+    delay: 1,
+    duration: 0.75,
+  }
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+  
   return(
     <section id="selected-work">
-      <h3>Selected Work</h3> 
+      <h3 className="clippy">
+        <motion.span {...animateProps} transition={transitionProps}>Selected Work</motion.span>
+        <span ref={ref}></span>
+      </h3> 
       <Project projectName="Loblaws" projectDesc={["Re-designing the digital experience of Canada's leading","supermarket chain with usability in mind"]} skills={["UX Research", "UI/UX Design", "Case Study"]} />
       <Project projectName="Cura" projectDesc={["Healthcare system bridging the distance between woman", "living in remote communities and accessible parental care", "using modern day biotechnology"]} skills={["UX Research", "UI/UX Design"]} inverted />
       <Project projectName="Presto" projectDesc={["Wearable concept for electronic payment system of Toronto’s", "public transit system"]} skills={["UI Design"]} />
